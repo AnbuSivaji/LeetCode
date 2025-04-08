67ee97b3420d5b30473de185c3ddc2c00f25894d
AnbuSivaji <anbusoft57@gmail.com>
2025-04-08 17:47:07 +0530

    Square

---
+++ Square.java
@@ -0,0 +1,8 @@
+
+public class Square {
+    public static void main(String[] args) {
+        double number = 5; // Example number to square
+        double result = Math.pow(number, 2); // Calculate the square using Math.pow
+        System.out.println("The square of " + number + " is: " + result);
+    }
+}
